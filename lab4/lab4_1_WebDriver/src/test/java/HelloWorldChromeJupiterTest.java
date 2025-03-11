66b8b0b1fc1298db7b0bff207d1f7a0c6bc3e91b
Ric <henriqueft04@ua.pt>
2025-03-11 16:50:45 +0000

    4.2 finished

--- lab4/lab4_1_WebDriver/src/test/java/HelloWorldChromeJupiterTest.java
+++ lab4/lab4_1_WebDriver/src/test/java/HelloWorldChromeJupiterTest.java
@@ -1,16 +1,21 @@
+import io.github.bonigarcia.seljup.SeleniumJupiter;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.commons.logging.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static java.lang.invoke.MethodHandles.lookup;
 import static org.junit.platform.commons.logging.LoggerFactory.getLogger;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(SeleniumJupiter.class)
 public class HelloWorldChromeJupiterTest {
     static final Logger log = getLogger(lookup().lookupClass());
 
@@ -36,6 +41,16 @@ public class HelloWorldChromeJupiterTest {
 
         // Verify
         assertThat(title).isEqualTo("Hands-On Selenium WebDriver with Java");
+
+        WebElement slowCalculator = driver.findElement(By.xpath("//a[contains(text(),'Slow calculator')]"));
+        slowCalculator.click();
+
+
+        String expectedUrl = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html";
+        String actualUrl = driver.getCurrentUrl();
+
+        log.debug(() -> String.format("Navigated to: %s", actualUrl));
+        assertThat(actualUrl).isEqualTo(expectedUrl);
     }
 
     @AfterEach
