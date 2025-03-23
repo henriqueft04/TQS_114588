e688acaeb4920a3ed9693e5bfaff62fc0d3d8776
Ric <henriqueft04@ua.pt>
2025-03-23 23:09:42 +0000

    4.1 done

--- lab6/lab6_1_TestContainer/src/test/java/tqs/lab6_1_testcontainer/CustomerServiceTest.java
+++ lab6/lab6_1_TestContainer/src/test/java/tqs/lab6_1_testcontainer/CustomerServiceTest.java
@@ -4,17 +4,15 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.MethodOrderer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import tqs.lab6_1_testcontainer.Customer;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -54,6 +52,28 @@ public class CustomerServiceTest {
     @Test
     @Order(2)
     void shouldRetrieveCustomers() {
-        // Implement test logic
+        Iterable<Customer> customers = customerRepository.findAll();
+        assertThat(customers).asList().hasSize(1);
+
+        Customer customer = customers.iterator().next();
+        assertThat(customer.getName()).isEqualTo("John");
+    }
+
+    @Test
+    @Order(3)
+    void shouldUpdateCustomer() {
+        Customer customer = customerRepository.findAll().iterator().next();
+        customer.setName("Johnny");
+        customerRepository.save(customer);
+
+        customer = customerRepository.findById(customer.getId()).get();
+        assertThat(customer.getName()).isEqualTo("Johnny");
+    }
+
+    @Test
+    @Order(4)
+    void shouldRetrieveUpdatedCustomer() {
+        Customer customer = customerRepository.findAll().iterator().next();
+        assertThat(customer.getName()).isEqualTo("Johnny");
     }
 }
